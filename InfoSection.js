2d2c9da7d4237a94971e88caaccf4a4377c8341b
Luka Kobal <info.radialart@gmail.com>
2025-12-03 06:41:05 +0100

    Update InfoSection.js

--- InfoSection.js
+++ InfoSection.js
@@ -1,1 +1,12 @@
+import React from "react";
+import ActionButton from "./ActionButton";
+
+export default function InfoSection({ onIncrement }) {
+  return (
+    <div className="info-box">
+      <h2>Child Component</h2>
+      <ActionButton onIncrement={onIncrement} />
+    </div>
+  );
+}
 
